697ba3a85a7db6f9feb251a54add2c9bb3ed9253
quyenhan13 <30314088+quyenhan13@users.noreply.github.com>
2026-05-09 11:19:59 +0700

    Auto update: Sat 05/09/2026 11:19:59.26

--- check_db.php
+++ check_db.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $pdo = new PDO('mysql:host=localhost;dbname=vteen_upload', 'root', '');
+    $pdo = new PDO('mysql:host=localhost;dbname=vteen', 'root', '');
     $stmt = $pdo->query("SHOW COLUMNS FROM vteen_guest_files LIKE 'user_id'");
     $res = $stmt->fetch();
     if ($res) {
